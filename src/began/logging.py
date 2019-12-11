a9b2f6569b1fcfe6302707bc84d1e94f989aab7d
Ben Thorne <bn.thorne@gmail.com>
2019-12-11 14:33:56 -0800

    Added logging submodule and remove logging function from vae.py

---
+++ src/began/logging.py
@@ -0,0 +1,28 @@
+import os
+import datetime
+from pathlib import Path
+import tensorflow as tf
+
+def setup_vae_run_logging(hpars):
+    """ Function to setup the logging for a given run. This requires the
+    environment variable TF_LOGDIR to exist. If it does not exist an error
+    will be thrown. 
+    
+    Parameters
+    ----------
+    
+    hyperparameters: dict
+        Dictionary containing the hyperparameters for this run. These are
+        latent dimension, batch size, and number of training epochs.
+        
+    Notes
+    -----
+    Will create logging directory if it does not exist.
+    """
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    logging_dir = Path(os.environ['TF_LOGDIR']) 
+    network_identifier = "LATDIM-{:03d}_BATCHSZ-{:03d}_EPOCHS-{:03d}".format(hpars['LAT_DIM'], hpars['BATCH_SIZE'], hpars['EPOCHS'])
+    network_logdir = logging_dir / network_identifier
+    network_logdir.mkdir(exist_ok=True)
+    summary_writer = tf.summary.create_file_writer(str(network_logdir / current_time))
+    return summary_writer

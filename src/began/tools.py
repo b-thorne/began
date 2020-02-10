Trying to fix problem in patch center calculation introduced by units

src/began/tools.py
@@ -135,9 +135,9 @@ def get_patch_centers(gal_cut: u.deg, step_size: u.deg):
     """
     gal_cut = gal_cut.to(u.deg)
     step_size = step_size.to(u.deg)
-    southern_lat_range = list(np.arange(-90 * u.deg, -gal_cut, step_size))
-    northern_lat_range = list(np.arange(gal_cut + step_size, 90 * u.deg, step_size))
-    lat_range = list(np.concatenate((southern_lat_range, northern_lat_range)))
+    southern_lat_range = np.arange(-90 * u.deg, -gal_cut, step_size)
+    northern_lat_range = np.arange(gal_cut + step_size, 90 * u.deg, step_size)
+    lat_range = np.concatenate((southern_lat_range, northern_lat_range))
 
     centers = []
     for t in lat_range: